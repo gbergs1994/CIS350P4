d131924c4441329d05707995875539287c10da25
gbergs1994 <144708640+gbergs1994@users.noreply.github.com>
2024-06-20 04:09:01 +0000

    message

--- Sorts.h
+++ Sorts.h
@@ -6,14 +6,13 @@ using namespace std;
 
 template <typename Comparable>
 
-class Sorts
-{
+class Sorts {
 private:
 
 
 public: 
 
-        static int insertionSort (vector <Comparable> & a) {
+    static int insertionSort (vector <Comparable> & a) {
         int numberCompares = 0;
 
         int j;
@@ -32,49 +31,101 @@ public:
     }
 
 
-    static int quicksortWithoutCutoff (vector <Comparable> & a) {
-        int numberCompares = 0;
+    static int quicksortWithoutCutoff(vector<Comparable> & a) {
         int left = 0;
-        int right = a.size();
-        int center = (left + right) / 2;
-        if (a[center] < a[left]) {
-            swap(a[left], a[center]);
-            numberCompares++;
-            }
-        if (a[right] < a[left]) {
-            swap(a[left], a[right]);
-            numberCompares++;
-            }
-        if (a[right] < a[center]) {
-            swap(a[center], a[right]);
-            numberCompares++;
-            }
-
-        // Place pivot at position right - 1
-        swap(a[center], a[right - 1]);
-        return a[right - 1];
+        int right = a.size()-1;
+        int numberCompares = 0;
+        numberCompares += quicksortWithoutCutoff(a, left, right);
         return numberCompares;
     }
 
 
-    static int quicksortWithCutoff (vector <Comparable> & a, int cutoff){
+    static int quicksortWithoutCutoff(vector<Comparable> & a, int left, int right) {
         int numberCompares = 0;
-        int left = 0;
-        int right = a.size();
 
+        //Base case: vector size 0 or 1 element
+        if (a.size() <= 1) {
+            return numberCompares;
+        }
+        
+        int center = (left + right) / 2;
+        if (a[center] < a[left]) {
+            swap(a[left], a[center]);
+            numberCompares++;
+        }
+        if (a[right] < a[left]) {
+            swap(a[left], a[right]);
+            numberCompares++;
+        }
+        if (a[right] < a[center]) {
+            swap(a[center], a[right]);
+            numberCompares++;
+        }
+        
+        //Base case: vector size 3 or fewer elements
+        if (a.size() <= 3) {
+            return numberCompares;
+        }
+
+        // Place pivot at position right - 1
+        swap(a[center], a[right - 1]);
+        numberCompares++;
+        Comparable pivot = a[right - 1];
+        // Begin partitioning
+        int i = left, j = right - 1;
+        for (;;) {
+            while (a[++i] < pivot)
+            {
+                numberCompares++;
+            }
+            while (pivot < a[--j])
+            {
+                numberCompares++;
+            }
+            if (i < j) {
+                swap(a[i], a[j]);
+                numberCompares++;
+            }
+            else
+                break;
+            }
+            swap(a[i], a[right - 1]); // Restore pivot
+            //quicksortWithoutCutoff(a); // Sort small elements
+            //quicksortWithoutCutoff(a); // Sort large elements
+            return numberCompares;
+    }
+
+
+    static int quicksortWithCutoff(vector<Comparable> & a, int cutoff) {
+            int left = 0;
+            int right = a.size()-1;
+            int numberCompares = 0;
+            numberCompares += quicksortWithCutoff(a, left, right, cutoff);
+            return numberCompares;
+    }
+
+
+    static int quicksortWithCutoff(vector<Comparable> & a, int left, int right, int cutoff) {
+        int numberCompares = 0;
+        
         if (left + cutoff <= right) {
-
             int center = (left + right) / 2;
-            if (a[center] < a[left])
+            if (a[center] < a[left]) {
                 swap(a[left], a[center]);
-            if (a[right] < a[left])
+                numberCompares++;
+            }
+            if (a[right] < a[left]) {
                 swap(a[left], a[right]);
-            if (a[right] < a[center])
+                numberCompares++;
+            }
+            if (a[right] < a[center]) {
                 swap(a[center], a[right]);
+                numberCompares++;
+            }
             // Place pivot at position right - 1
             swap(a[center], a[right - 1]);
+            numberCompares++;
             Comparable pivot = a[right - 1];
-
             // Begin partitioning
             int i = left, j = right - 1;
             for (;;) {
@@ -93,20 +144,16 @@ public:
                 else
                     break;
             }
-
             swap(a[i], a[right - 1]); // Restore pivot
-
-            quicksortWithCutoff(a, cutoff); // Sort small elements
-            quicksortWithCutoff(a, cutoff); // Sort large elements
-            
+            numberCompares++;
+            //quicksortWithCutoff(a, cutoff); // Sort small elements
+            //quicksortWithCutoff(a, cutoff); // Sort large elements
             return numberCompares;
-            
         }
         else // Do an insertion sort on the subarray
             insertionSort(a);
-        
-        
         return numberCompares;
     }
+
 
 };
